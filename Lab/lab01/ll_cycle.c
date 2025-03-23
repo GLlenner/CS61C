01143ac02402d7b2a897cbe411a913e0c810ae6d
Vida <2918659582@qq.com>
2025-03-23 15:38:48 +0800

    lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if(head == NULL){
+    	return 0;
+    }
+    node *torotise = head;
+    node *hare = head->next;
+    
+    while(hare != NULL && hare->next != NULL ){
+    	if(torotise == hare){
+	   return 1;
+	}
+	hare = hare->next->next;
+	torotise = torotise->next;
+    }
     return 0;
 }
