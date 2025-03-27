2a738b8283bcf8e935fb9d61fbeb5c722650004c
liang.gong <liang.gong@mthreads.com>
2025-03-27 17:47:57 +0800

    temp

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -6,5 +6,14 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t _0th = 1 & (*reg);
+    uint16_t _2th = 1 & (*reg >> 2);
+    uint16_t _3th = 1 & (*reg >> 3);
+    uint16_t _5th = 1 & (*reg >> 5);
+
+    uint16_t add  = _0th ^ _2th ^ _3th ^ _5th;
+
+    *reg = *reg >> 1;
+    *reg = *reg | (add << 15);
 }
 
